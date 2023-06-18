5bcbd1f0e50050c92ad0694b4fd8123171edc076
creatmylife123 <1224332854@qq.com>
2023-06-18 17:33:11 +0800

    test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,10 +120,11 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          独树一帜的小店
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        <a href="https://www.openaiforever.top/">店铺地址：点击此处跳转</a>
+
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
